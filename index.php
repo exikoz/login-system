6234fdf05ae8cb4c74a3a7ef76ca6577fc59bbd4
Hussein <hussein.hasnawy@gmail.com>
2019-01-25 19:23:21 +0100

    Add login form

--- index.php
+++ index.php
@@ -1,1 +1,45 @@
-<?php echo "Hello world" ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/css/uikit.min.css" />
+        <!-- UIkit JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+
+        <div class="uk-section uk-container">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+                <form class="uk-form-stacked" js-login>
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Email</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="your@email.com">
+                        </div>
+                    </div>
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-text">Password</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="password">
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-default" type="submit">LOGIN</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+
+        <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+    </body>
+</html>
