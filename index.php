76496452b702add11b22c3b041c7b67124b81fea
Hussein <hussein.hasnawy@gmail.com>
2019-01-25 20:03:04 +0100

    Added config: added: login/reg pages: added: footer

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+
+<?php 
+    
+    // Allow the config
+    define('__CONFIG__', true);
+    // Require the config
+    require_once "inc/config.php"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,40 +16,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- UIkit CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/css/uikit.min.css" />
-        <!-- UIkit JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.2/js/uikit-icons.min.js"></script>
     </head>
     <body>
 
         <div class="uk-section uk-container">
-            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
-                <form class="uk-form-stacked" js-login>
-
-                    <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Email</label>
-                        <div class="uk-form-controls">
-                            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="your@email.com">
-                        </div>
-                    </div>
-                    <div class="uk-margin">
-                        <label class="uk-form-label" for="form-stacked-text">Password</label>
-                        <div class="uk-form-controls">
-                            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="password">
-                        </div>
-                    </div>
-
-                    <div class="uk-margin">
-                        <button class="uk-button uk-button-default" type="submit">LOGIN</button>
-                    </div>
-
-                </form>
-            </div>
+            <?php 
+                echo "Hello World Today is: ";
+                echo date("Y m d");
+            ?>
+            <p>
+                <a href="/login-form/php_login_course/login.php">Login</a>
+                <a href="/login-form/php_login_course/register.php">Register</a>
+            </p>
         </div>
 
-        <script
-        src="https://code.jquery.com/jquery-3.3.1.js"
-        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-        crossorigin="anonymous"></script>
+        <?php include_once "inc/footer.php"; ?>
     </body>
 </html>
